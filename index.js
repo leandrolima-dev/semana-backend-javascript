a19a8789281ef7e10c4200e6f385d5268deb7a0d
Leandro Lima <67746908+leandrolima-dev@users.noreply.github.com>
2023-10-17 22:29:28 +0000

    feat: implement `express` into `index.js` with default endpoint

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("JavaScript rodando!");
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send("Hello World!")
+});
+
+app.listen(3000)
