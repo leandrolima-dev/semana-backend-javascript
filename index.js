bc9d9c5cfc0cddbcaf2a195ea884e4b2da6ad988
Leandro Lima <67746908+leandrolima-dev@users.noreply.github.com>
2023-10-18 22:21:50 +0000

    feat: implement endpoint `Update`  `[PUT] /items/:id`

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 // Sinalizar para o Express que o corpo
 // das requisições estará sempre em JSON
-app.use(express.json())
+app.use(express.json());
 
 app.get('/', function (req, res) {
     res.send("Hello World!")
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/oi', function (req, res) {
     res.send("Olá mundo!")
-})
+});
 
 // CRUD de lista de DevMon
 
@@ -24,10 +24,15 @@ app.get("/items", function (req, res) {
 
 // READ BY ID - [GET] /items/:id
 app.get("/items/:id", function (req, res) {
+    // Acessamos o parâmetro de rota ID
+    // Subtraímos 1 para corrigir a questão do índice
+    // da lista que começa em 0
     const id = req.params.id - 1;
 
+    // Acessamos o item na lista a partir do index
     const item = items[id]
 
+    // Exibimos o item obtido
     res.send(item)
 });
 
@@ -41,7 +46,22 @@ app.post("/items", function (req, res) {
 
     //Enviamos uma msg de sucesso
     res.send("Item created sucessfuly!")
-})
+});
+
+// UPDATE - [PUT] /items/:id
+app.put("/items/:id", function (req, res) {
+    // Acessamos o parâmetro de rota e corrigimos o índice
+    const id = req.params.id - 1
+  
+    // Obtemos o novo item a partir do corpo da requisição
+    const newItem = req.body.name
+  
+    // Colocamos o novo item na mesma posição do item anterior
+    items[id] = newItem
+  
+    // Enviamos uma mensagem de sucesso
+    res.send("Item updated successfully.")
+});
 
 app.listen(3000, () => {
     console.log("App running on http://localhost:3000");
