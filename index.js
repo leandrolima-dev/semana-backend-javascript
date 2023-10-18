049b73dc9f61cd4aa534f572e22b6bc73f2d350e
Leandro Lima <67746908+leandrolima-dev@users.noreply.github.com>
2023-10-18 22:10:56 +0000

    feat: implement endpoint `Create [POST]` /`items`

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+// Sinalizar para o Express que o corpo
+// das requisições estará sempre em JSON
+app.use(express.json())
+
 app.get('/', function (req, res) {
     res.send("Hello World!")
 });
@@ -16,6 +20,18 @@ const items = ["Java", "Android", "Kotlin", "Express", "NestJS"];
 // READ ALL - [GET] /items
 app.get("/items", function (req, res) {
     res.send(items)
+});
+
+// CREAT - [POST] /items
+app.post("/items", function (req, res) {
+    //Extraio a informação do corpo da requisição
+    const item = req.body.name
+
+    //Insiro o item na lista
+    items.push(item)
+
+    //Enviamos uma msg de sucesso
+    res.send("Item created sucessfuly!")
 })
 
 app.listen(3000, () => {
