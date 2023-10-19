ecf24974583a6efcfa11e41d2b2564226db21766
Leandro Lima <67746908+leandrolima-dev@users.noreply.github.com>
2023-10-19 23:57:41 +0000

    feat: set port to use `process.env.PORT`

--- index.js
+++ index.js
@@ -150,8 +150,10 @@ async function main() {
         res.send("Item deleted successfully.")
     });
 
-    app.listen(3000, () => {
-        console.log("App running on http://localhost:3000");
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+        console.log(`App running on http://localhost:${port}`);
     })
 };
 
