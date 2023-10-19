0ef2e1e5238d4be46673904313f9fa8caa5c2e67
Leandro Lima <67746908+leandrolima-dev@users.noreply.github.com>
2023-10-19 23:36:15 +0000

    feat: implement database on endpoint Read by Id

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const url = "mongodb+srv://admin:18mon01go06db@cluster0.ckc9xh5.mongodb.net";
 const client = new MongoClient(url);
@@ -54,15 +54,20 @@ async function main() {
     });
 
     // READ BY ID - [GET] /items/:id
-    app.get("/items/:id", function (req, res) {
+    app.get("/items/:id", async function (req, res) {
         // Acessamos o parâmetro de rota ID
         // Subtraímos 1 para corrigir a questão do índice
         // da lista que começa em 0
-        const id = +req.params.id;
+        const id = req.params.id;
 
         // Acessamos o item na lista a partir do index
-        const item = items.find(function (element) {
-            return element.id === id
+        // const item = items.find(function (element) {
+        //     return element.id === id
+        // })
+
+        //Buscamos o documento na collection
+        const item = await collection.findOne({
+            _id: new ObjectId(id)
         })
 
         // Exibimos o item obtido
